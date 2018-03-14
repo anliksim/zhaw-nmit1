674c71484ac06581a2f1d29b6f64826d4275cc84
Simon Anliker <anliksim@users.noreply.github.com>
2018-03-14 19:26:39 +0100

    correction for eps calc in ex2

--- exercise2/Anliker_Simon_IT16ta_ZH09_S2_Aufg4.m
+++ exercise2/Anliker_Simon_IT16ta_ZH09_S2_Aufg4.m
@@ -1,11 +1,8 @@
 % calculate eps and save to variable eps
 
-% if eps is 0 set to 1
-if(1 + eps ~= 1)
-    eps = 1;
-end
-
 % divide by 2 util we reach eps
-while(eps/2 ~= 0)
+eps = 1;
+while 1 + eps ~= 1
     eps = eps/2;
 end
+eps = eps * 2;
