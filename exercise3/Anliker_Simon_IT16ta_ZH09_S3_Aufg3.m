7778365e696fce7b14ce466190589d94d129faa2
Simon Anliker <anliksim@users.noreply.github.com>
2018-03-19 18:03:38 +0100

    ex3 aufg3: add decimal place calculation and string inversion

--- exercise3/Anliker_Simon_IT16ta_ZH09_S3_Aufg3.m
+++ exercise3/Anliker_Simon_IT16ta_ZH09_S3_Aufg3.m
@@ -3,7 +3,11 @@ function [y,value,abs_err,rel_err] = Anliker_Simon_IT16ta_ZH09_S3_Aufg3(x,B,nmax
 % B: Basis 1 < B < 10
 % nmax: max Nachkommastellen
 % ...
-% Example: [y,value,abs_err,rel_err] = Anliker_Simon_IT16ta_ZH09_S3_Aufg3(x,B,nmax)
+% Example: [y,value,abs_err,rel_err] = Anliker_Simon_IT16ta_ZH09_S3_Aufg3(7.44, 5, 2)
+
+if (1 >= B) && (B >= 10)
+   error('Basis B must be between 1 and 10 exclusive'); 
+end
 
 % Ganzzahliger Anteil
 z1 = abs(fix(x));
@@ -13,23 +17,63 @@ z2 = abs(x) - z1;
 % Hilfsvariablen
 div = z1;
 count = 0;
-y(1) = '0';
+dummy1(1) = '0';
 dummy2(1) = '0';
 
-
-while div > 0
-    count = count + 1;
+% Solange der Quotient nicht 0 ist
+while div > 0 
+    % Wiederhole für nächste Stelle
+    count = count + 1; 
+    % Teile Zahl mit Rest durch Basis B
     rest = mod(div, B);
-    y(count) = num2str(rest); % Schreibe rest in dummy and der Stelle count
-    div = fix(div/B);
+    % Schreibe Rest in dummy1 and der Stelle count
+    dummy1(count) = num2str(rest);
+    % Rest der Division ist nächste Ziffer
+    div = fix(div/B); 
 end
 
-value = 0;
-abs_err = 0;
-rel_err = 0;
+div = z2;
+count = 0;
 
-% dummy1 - reihenfolge beachten
+% Solange Rest nicht 0 oder bis nmax erreicht ist
+while (div ~= 0) && (length(dummy2) ~= nmax)
+    % Wiederhole für nächste Stelle
+    count = count + 1;
+    % Multipliziere Zahl mit Basis B
+    res = div * B;
+    % Ganzzahliger Anteil
+    res_z1 = abs(fix(res));
+    % Schreibe ganzzahliger Anteil in dummy2 and der Stelle count
+    dummy2(count) = num2str(res_z1);
+    % Nachkomma Anteil ist nächste Ziffer
+    div = abs(res) - res_z1;
+end
 
-% same für Nachkommaanteil
+value = 0; % TODO
+abs_err = 0; % TODO
+rel_err = 0; % TODO
+
+% Invertiert dummy1 um das richtige Resultat zu erhalten
+inv_d1 = inverse_string(dummy1);
+
+% Füge die Vor- und Nachkommastellen zusammen
+y = strcat(inv_d1,'.',dummy2);
+
+end
+
+
+function [inv_str] = inverse_string(str)
+% Nimmt einen beliebigen String und kehrt ihn um.
+%
+% str: zu invertierende string Sequenz
+% Example: [inv_str] = inverse_string('123')
+
+len = length(str);
+idx = len+1;
+inv_str(len) = '0';
+
+for i = 1:len
+    inv_str(i) = str(idx-i);
+end
 
 end
