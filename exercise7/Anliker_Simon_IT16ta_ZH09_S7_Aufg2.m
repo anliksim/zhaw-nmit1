ce73bc552d876c283f985a1134d2c9c4336d14dd
Simon Anliker <anliksim@users.noreply.github.com>
2018-04-16 17:46:05 +0200

    ex7 comments

--- exercise7/Anliker_Simon_IT16ta_ZH09_S7_Aufg2.m
+++ exercise7/Anliker_Simon_IT16ta_ZH09_S7_Aufg2.m
@@ -3,8 +3,8 @@ function [A_triangle,detA,x] = Anliker_Simon_IT16ta_ZH09_S7_Aufg2(A,b)
 % des Vektors x. Gibt zusätzlich die Determinante von A (detA) sowie die
 % obere Dreiecksmatrix von A (A_triangle) zurück. 
 %
-% A: Matrix mit Dimension m*n bzw 
-% b: Spaltenvektor mit m Zeilen
+% A: Matrix mit Dimension n*n bzw 
+% b: Spaltenvektor mit n Zeilen
 % Example: [A_triangle,detA,x] = Anliker_Simon_IT16ta_ZH09_S7_Aufg2([200 150 100;50 30 20;20 10 0],[2150;470;150])
 
 % Determinante A <=> -1*det(A_triangle)
@@ -22,6 +22,7 @@ for i = 1:n-1
     end
     
     for j = i+1:n
+        % Zeile j := Zeile j - a_ji/a_ii * Zeile i
         A(j,:) = A(j,:) - (A(j,i)./A(i,i)).*A(i,:);
     end
     
@@ -33,10 +34,12 @@ A_triangle = A(:,1:n);
 % x berechnen durch Rückwärtseinsetzen
 x = zeros(3,1);
 for i = n:-1:1
+    % Summer aller a_ij * x_j
     sum = 0;
     for j = i+1:n
       sum = sum + A(i,j) * x(j);
     end
+    % b_i minus Summe durch a_ii
     x(i) = (A(i,n+1) - sum) / A(i,i);
 end
 
